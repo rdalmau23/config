595ba30e88df62794a59954e83e1a4da3305df8c
rdalmau23 <rdalmau23@ilg.cat>
2025-04-02 18:36:28 +0200

    feat: creació de formulari

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Usuari(models.Model):
+    id = models.AutoField(primary_key=True)
+    email = models.EmailField(unique=True)
+    nom = models.CharField(max_length=100)
+    password = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.nom
--- app/views.py
+++ app/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .models import Usuari
+from .forms import LoginForm
+
+def login(request):
+    error = None
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            usuari = Usuari.objects.filter(email=email, password=password).first()
+            if usuari:
+                return render(request, './home.html', {"usuari": usuari})
+            else:
+                error = "Credencials incorrectes"
+    else:
+        form = LoginForm()
+    return render(request, './login.html', {"form": form, "error": error})
