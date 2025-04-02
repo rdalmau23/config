9ed6f271725c28f9299c651b59e296f93f7e03c3
rdalmau23 <rdalmau23@ilg.cat>
2025-04-02 19:01:20 +0200

    feat: login amb sessió

--- app/views.py
+++ app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render
-from .models import Usuari
-from .forms import LoginForm
+from django.shortcuts import redirect
+from django.contrib.auth import login, logout, authenticate
+
+from app.forms import LoginForm
+from app.models import Usuari
+
 
 def login(request):
     error = None
@@ -14,9 +17,15 @@ def login(request):
             password = form.cleaned_data['password']
             usuari = Usuari.objects.filter(email=email, password=password).first()
             if usuari:
-                return render(request, './home.html', {"usuari": usuari})
+                request.session['usuari_id'] = usuari.id
+                return redirect('inici')
             else:
                 error = "Credencials incorrectes"
     else:
         form = LoginForm()
     return render(request, './login.html', {"form": form, "error": error})
+
+def logout_view(request):
+    request.session.flush()
+    return redirect('login')
+
